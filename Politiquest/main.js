3f38066b8926fbbcf13201fc4456d77309e9ceaf
HJAgresta <hjagresta@gmail.com>
2017-02-11 16:24:20 -0500

    main
    chages

--- Politiquest/main.js
+++ Politiquest/main.js
@@ -2,10 +2,11 @@ function init()
 {	
 	var canvas = document.querySelector("#canvas")
 	var ctx = canvas.getContext('2d');
-	
+	ctx.textAlign = "center";
+	ctx.textBaseline = "middle";
 	
 	ctx.fillStyle = 'black';
-	ctx.fillRect(35,50,50,50);
+	ctx.fillRect(350,260,100,80);
 };
 
 window.onload=init;
