b538b182cac7b4054b6d7eb06bc77cc1ad04fe24
HJAgresta <hjagresta@gmail.com>
2017-02-11 21:11:11 -0500

    added questions
    questions

--- Politiquest/questions.js
+++ Politiquest/questions.js
@@ -4,8 +4,414 @@
 var app = app || {};
 
 app.questions = {
-    var questions[];
-    var easy[];
-    var moderate[];
-    var hard[];
+	states: [
+		{ name: "Alabama", questions:
+			[
+			{Q: "What is the capital of Alabama", A: "Montgomery", B:"Montpelier", C:"Ankara", D:"New Cambridge", Answer: 1}
+			{Q: "", A: "", B:"", C:"", D:"", Answer: : ""}
+			{Q: "", A: "", B:"", C:"", D:"", Answer: : ""}
+			{Q: "", A: "", B:"", C:"", D:"", Answer: : ""}
+			{Q: "", A: "", B:"", C:"", D:"", Answer: : ""}
+		]},
+		{ name: "Alaska", questions:
+		[
+		{Q: "What is the current offical and controversial name of tallest mountain in Alaska?", A: "McKinley", B:"Denali", C:"Denora", D:"Vesuvius", Answer: 2}
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		]},
+		{ name: "Arizona", questions:
+		[
+		{Q: "How long has Arizona been in a drought for according to the Arizona State Climate Office?", A: "5 years", B:"21 Years", C:"101 Years", D:"1006", Answer: 2}
+		{Q: "What is Allepo", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		]},
+		{ name: "Arkansas", questions:
+		[
+		{Q: "What agriculture does Arkansas produce the most of?", A: "Rice, Poultry, Cotton", B:"Potatoes, Sardines, Indigo", C:"Wheat, Salmon, Sunflowers", D:"Barley, Cheese, Pine Trees", Answer: 1}
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		]},
+		{ name: "California", questions:
+		[
+		{Q: "How much is the production of marijuana taxed in this state?", A: "$5.25/ounce", B:"$9.25/ounce", C:"$11/ounce", D:"$15.33/ounce", Answer: 2}
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		]},
+		{ name: "Colorado", questions:
+		[
+		{Q: "Why was Fort Collins built?", A: "For the War of 1812", B:"For the war started in 1776", C:"By Squatters to fend off Speculators", D:"For the Indian Wars", Answer: 4}
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		]},
+		{ name: "Connecticut", questions:
+		[
+		{Q: "What president was born here?", A: "James K. Polk", B:"Herbert Hoover", C:"Georege W. Bush", D:"John Adams", Answer: 3}
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		]},
+		{ name: "Delaware", questions:
+		[
+		{Q: "What ocean does Delaware touch?", A: "Pacific", B:"Artic", C:"Indian", D:"Atlantic", Answer: 4}
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		]},
+		{ name: "Florida", questions:
+		[
+		{Q: "Why was the Kennedy Space Center built here?", A: "So rockets don't end up going over populated areas", B:"Healthy tax exemptions", C:"Florida won it in a bid", D:"Eisenhower had a fondness for the state", Answer: 3}
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		]},
+		{ name: "Georgia", questions:
+		[
+		{Q: "Around how many tons of peaches does Georgia produce a year?", A: "20,000 tons", B:"40,000 tons", C:"60,000 tons", D:"80,000 tons", Answer: 2}
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		]},
+		{ name: "Hawaii", questions:
+		[
+		{Q: "How many main islands make up Hawaii", A: "6", B:"7", C:"8", D:"9", Answer: 3}
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		]},
+		{ name: "Idaho", questions:
+		[
+		{Q: "What does Idaho mean?", A: "The Sun Comes Down the Mountain", B:"Fertile Earth", C:"A declaration of promiscuous behavior", D:"Land of the strong deer", Answer: 1}
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		]},
+		{ name: "Illinois", questions:
+		[
+		{Q: "What is a chicago typewriter?", A: "A hot dog", B:"A news Paper", C:"A typewriter", D:"A gun", Answer: 4}
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		]},
+		{ name: "Indiana", questions:
+		[
+		{Q: "How many laps in the Indy 500?", A: "50", B:"150", C:"200", D:"500", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		]},
+		Iowa: 
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		Kansas:
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		Kentucky:
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		Louisiana:
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		Maine:
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		Maryland:
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		Massachusets:
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		Michigan:
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		Minnesota:
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		Mississippi: 
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		Missouri:
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		Montana:
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		Nebraska:
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		Nevada:
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		New Hampshire:
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		New Jersey:
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		New Mexico:
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		New York:
+		[
+		{Q: "How many presidents were born in New York state?", A: "3", B:"4", C:"5", D:"6", Answer: B:"4"}
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		North Carolina:
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		North Dakota:
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		Ohio: 
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		Oklahoma:
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		Oregon:
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		Pennsylvania:
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		Rhode Island:
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		South Carolina:
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		South Dakota:
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		Tennessee:
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		Texas:
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		Utah:
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		Vermont:
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		Virginia:
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		Washington:
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		West Virginia:
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		Wisconsin:
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		Wyoming:
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		],
+		DistrictofColumbia:
+		[
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		{Q: "", A: "", B:"", C:"", D:"", Answer: }
+		]
+	]
 };
